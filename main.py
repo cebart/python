2f16cd5d046caef891e62c60d38ff9efaef6c77f
cebart <cbartoli@protonmail.com>
2022-07-13 13:05:57 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-test
+def voyToLetter(src):
+    ret = [src]
+    for letter in changes:
+        buf = ''
+        checkDouble = 1
+        for c in src:
+            if (c in voy):
+                c = letter
+            buf += c
+        for r in ret:
+            if r == buf:
+                checkDouble = 0
+        if checkDouble:
+            ret.append(buf)
+    return (ret)
+
+word = 'bobi'
+changes = ['i', 'a', 'u', 'y']
+voy = ['i', 'a', 'u', 'y', 'e', 'o']
+rez = []
+
+rez += voyToLetter(word)
+for result in rez:
+    print(result)
+
